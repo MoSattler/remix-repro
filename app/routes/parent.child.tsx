17ea2bbf9f6d66a7c1d8eb013635585efa73de3e
Mo Sattler <hi@mosattler.com>
2023-11-14 12:22:08 +0100

    Add cart modal component

--- app/routes/parent.child.tsx
+++ app/routes/parent.child.tsx
@@ -1,7 +1,7 @@
 import { json } from "@remix-run/node";
-import { Form, Outlet, useActionData, Link } from "@remix-run/react";
+import { Form, useActionData } from "@remix-run/react";
 
-const FILE_NAME = "parent.tsx";
+const FILE_NAME = "parent.child.tsx";
 
 export function action() {
   return json({ value: FILE_NAME });
@@ -11,19 +11,6 @@ export default function Parent() {
   let data = useActionData<typeof action>();
   return (
     <div>
-      Repro notes:
-      <ol>
-        <li>
-          Submit <b>parent.tsx</b> (and notice that it posts to the correct
-          action handler)
-        </li>
-        <li>
-          <Link to="/parent/child">Go to /parent/child</Link>
-        </li>
-        <li>
-          Submit <b>parent.child</b> and notice that <b>parents.tsx</b> also updates to data from <b>parent.child.tsx</b>
-        </li>
-      </ol>
       <h1>{FILE_NAME}</h1>
       <Form method="post">
         <button type="submit">Submit</button>
@@ -44,7 +31,6 @@ export default function Parent() {
           "has not posted yet"
         )}
       </pre>
-      <Outlet />
     </div>
   );
 }
